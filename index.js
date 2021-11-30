246ba55bf18cade3f920665030b370463b27059e
loren <lorenmichaelh@gmail.com>
2021-11-29 21:35:31 -0600

    commit tree 45de07052762de5f2795a94498b5e1642b7fb399

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function superbowlWin(array) {
+  const winYear = array.find(record => record.result === 'W');
+  if(winYear === undefined){
+    return undefined;
+  } else {
+    return winYear.year;
+  }
+};
